d2b48e3677468fc4dbe8e4b61c4bb39f42133944
Jared Rader <raderj89@gmail.com>
2014-05-23 11:34:49 -0400

    adding gemfile.lock

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+
+  root "pages#index"
+
+  post 'search', to: 'pages#search', as: 'search'
+  post 'search_youtube', to: 'pages#search_youtube', as: 'search_youtube'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
