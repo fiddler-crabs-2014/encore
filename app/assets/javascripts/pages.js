d78a0080b3a0019afff15ef51127e7a9a62567cd
Timmy Huang <timmyhuang0404@gmail.com>
2014-05-24 16:42:15 -0400

    update two files from Onaracs/encore_rails

--- app/assets/javascripts/pages.js
+++ app/assets/javascripts/pages.js
@@ -1,23 +1,22 @@
-songsAndIds = {}
+var songsAndIds = {};
 $(document).ready(function() {
-  $('.add-song-btn').on("click",function(event) {
-    event.preventDefault();
+  $('.add-song').on("click", function(e) {
+    e.preventDefault();
 
     var $this = $(this);
-
     var songId = $this.find('.song_id').val();
     var songTitle = $this.find('.song').val();
+  })
 
-    console.log(songId);
-
-    // songsAndIds[songId] = songTitle
-    // console.log(songsAndIds);
+  $("#save_concert").on("click", function(e){
+    e.preventDefault();
+    var concertInfo = $(this).closest("form").serialize();
+    var songList = $.param({ songs: songsAndIds });
+    $.ajax({
+      url: "create_concert",
+      method: "post",
+      data: (concertInfo + '&' + songList)
+    });
   })
 })
-
-
-
-
-
-
 
--- app/controllers/concerts_controller.rb
+++ app/controllers/concerts_controller.rb
@@ -3,20 +3,16 @@ class ConcertsController < ActionController::Base
   end
 
   def create
-    p "GOT YOU"
-    p params
-    # Suggest using first_or_create
+    songs = params[:songs]
     artist = Artist.where(name: params[:band]).first_or_create
     venue = Venue.where(name: params[:venue], city: params[:city], state: params[:state]).first_or_create
     concert = Concert.where(date: params[:date], venue_id: venue.id).first_or_create
     artist.concert_artists.create(concert_id: concert.id, artist_id: artist.id ,concert_position: "test")
     artist.concerts
-    if venue.save
-      p "VENUE SAVED"
-      p "VENUE ID is: #{venue.id}"
-    else
-      "ERROROROROROROROROROR"
-    end
 
+    songs.each do |s|
+      song = Song.where(title: s[1], artist_id: artist.id).first_or_create
+      song.concert_songs.create(concert_id: concert.id, song_id: song.id, video_identifier: s[0])
+    end
   end
 end
