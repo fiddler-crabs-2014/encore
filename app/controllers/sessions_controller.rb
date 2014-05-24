8562b730f115a6d3534ef4ca8bbc68b9cc6ed562
Brendan <brendan.scarano@gmail.com>
2014-05-24 19:48:52 -0400

    removed binding.pry from sessions controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,7 +1,6 @@
 class SessionsController < ApplicationController
   def create
     user = User.from_omniauth(env["omniauth.auth"])
-    binding.pry
     session[:user_id] = user.id
     redirect_to root_path
   end
