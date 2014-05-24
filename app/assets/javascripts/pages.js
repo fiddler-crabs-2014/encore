548549d70e62ff8a94f3642af2c0eaa4494f0b3d
Timmy Huang <timmyhuang0404@gmail.com>
2014-05-24 15:38:26 -0400

    delete comments in js

--- app/assets/javascripts/pages.js
+++ app/assets/javascripts/pages.js
@@ -19,14 +19,10 @@ $(document).ready(function() {
     var songList = $.param({ songs: songsAndIds });
     console.log("CAN I DO THIS?????");
     console.log(songsAndIds);
-    // console.log($.param(songsAndIds));
-    // console.log($.param({ song: { "song1": 1, "song2": 2 } }));
-    // console.log("CAN I DO THIS?????");
     console.log(concertInfo + '&' + songList);
     $.ajax({
       url: "create_concert",
       method: "post",
-      // data: $.param({ song: { "song1": 1, "song2": 2 } })
       data: (concertInfo + '&' + songList)
     });
   })
