b24dcde748a57cd80ec41f2f30379556fffc4cb0
Brendan <brendan.scarano@gmail.com>
2014-05-25 14:17:19 -0400

    Began model testing for:   - Concerts   - Venues   - Concert_songs

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,7 @@
 class User < ActiveRecord::Base
+  validates :email, presence: true
+  validates :email, uniqueness: true
+
   def self.from_omniauth(auth)
     where(auth.slice(:provider, :uid)).first_or_initialize.tap do |user|
       user.provider = auth.provider
