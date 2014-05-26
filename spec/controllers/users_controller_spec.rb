c30edf8a1c18585b2871caee6a883a6a752979d9
Timmy Huang <timmyhuang0404@gmail.com>
2014-05-26 00:11:33 -0400

    add 3 examples. All pass.

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -1,5 +1,30 @@
-# require 'spec_helper'
+require 'spec_helper'
 
-# describe UsersController do
+describe UsersController do
+  let!(:u1) { FactoryGirl.create(:user) }
+  let!(:u2) { FactoryGirl.create(:user) }
+  let!(:c1) { FactoryGirl.create(:concert) }
+  let!(:c2) { FactoryGirl.create(:concert) }
+  before { u1.concerts << c1 }
+  before { u1.concerts << c2 }
 
+  describe "GET #index" do
+    it "assigns all users to @users" do
+      get :index
+      assigns(:users).length.should eq(2)
+    end
+  end
 
+  describe "GET #show" do
+    it "assigns user to @user" do
+      get :show, id: u1.id
+      assigns(:user).id.should eq(u1.id)
+    end
+
+    it "assigns user's attended concerts to @concerts" do
+      get :show, id: u1.id
+      assigns(:concerts)[0].id.should eq(c1.id)
+      assigns(:concerts)[1].id.should eq(c2.id)
+    end
+  end
+end
