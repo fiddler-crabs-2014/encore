b9bf9fb37864a3016d9c7e499d5055ac6c4c27d5
Jared Rader <raderj89@gmail.com>
2014-05-25 23:03:39 -0400

    fix up model spec concert_song_spec

--- spec/models/concert_song_spec.rb
+++ spec/models/concert_song_spec.rb
@@ -2,27 +2,12 @@ require 'spec_helper'
 
 describe ConcertSong do
 
+  it { should validate_presence_of(:video_identifier) }
+
   describe "associations" do
     it { should belong_to(:concert) }
     it { should belong_to(:song) }
     it { should validate_uniqueness_of(:video_identifier).scoped_to(:concert_id) }
-  end
-
-  it "has a valid factory" do
-    FactoryGirl.create(:concert_song).should be_valid
-  end
-
-  it "is invalid without a concert_id" do
-    FactoryGirl.build(:concert_song, concert_id: nil).should_not be_valid
-  end
-
-  it "is invalid without a song_id" do
-    FactoryGirl.build(:concert_song, song_id: nil).should_not be_valid
-  end
-
-
-  it "is invalid without a video_identifier" do
-    FactoryGirl.build(:concert_song, video_identifier: nil).should_not be_valid
   end
 
 end
