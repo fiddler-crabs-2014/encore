54e02d889e173ae259e7ae9dae9721ca89ca7fb1
Jared Rader <raderj89@gmail.com>
2014-05-25 21:56:01 -0400

    Add check for multiple artists in MusicBrainz search - It's a hack now - we assume only one of the artists returned   is actually popular, so we iterate through each musicbrainz artist id   and return the first one that results in a valid response from a setlist.fm search - Need a better solution

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,12 +3,13 @@ class PagesController < ApplicationController
   end
 
   def search
-    @band = params[:band]
+    band_query = params[:band]
 
-    mbid = Musicbrainz.search(@band)
-    if mbid
-      @results = Setlist.search(mbid)
-      Artist.where(name: @band).first_or_create
+    mb_result = Musicbrainz.search(band_query)
+
+    if mb_result.is_a? String
+      @results = Setlist.search(mb_result)
+      @band = Artist.where(name: band_query).first_or_create
     else
       flash[:warning] = "Sorry - we couldn't find an artist with that name."
       render :index
@@ -17,20 +18,23 @@ class PagesController < ApplicationController
 
   def search_youtube
     @band = Artist.where(name: params[:band]).first_or_create
+
     search_params = params[:concert].split(', ')
+
     @venue = Venue.where(name: search_params[2], city: search_params[3], state: search_params[4]).first_or_create
     @concert = Concert.where(date: search_params[0], venue_id: @venue.id).first_or_create
     @concert_artist = ConcertArtist.where(concert_id: @concert.id, artist_id: @band.id).first_or_create
+
     @songs = params[:songs]
     @date = @concert.date.strftime('%B %e %Y')
     @tour = search_params[1]
-    #@venue = venue.name
-    #@city = venue.city.strftime
-    #@state = venue.state
 
     search1 = "#{@band.name}, #{@venue.name}, #{@venue.state}, #{@date}"
     search2 = "#{@band.name}, #{@venue.name}, #{@date}"
     search3 = "#{@band.name}, #{@venue.state}, #{@date}"
+    # A couple more search options
+    # search4 = "#{@band.name}, #{@tour}, #{@date}"
+    # search5 = "#{@band.name}, #{@tour}, #{@venue.name}"
 
     @titles_ids = {}
 
@@ -49,9 +53,5 @@ class PagesController < ApplicationController
         # @ids << result[/\(\w*\)\z/].gsub(/\(*\)*/, '')
       end
     end
-  end
-
-  def make_concert
-
   end
 end
--- app/services/musicbrainz.rb
+++ app/services/musicbrainz.rb
@@ -1,9 +1,21 @@
+require 'net/http'
 class Musicbrainz
 
   def self.search(artist)
     mb_results = MusicBrainz::Artist.search(artist)
     if mb_results.empty?
       return false
+    # In rare cases, MusicBrainz returns arrays containing multiple artists
+    # This conditional assumes only one of the artists will return results
+    # from setlist.fm. Definitely not foolproof - need a better solution
+    elsif mb_results.length > 1
+      mb_results.each do |mb_result|
+        mbid = mb_result[:mbid]
+        result = Net::HTTP.get_response(URI.parse(
+                  "http://api.setlist.fm/rest/0.1/artist/#{mbid}/setlists.json")
+                ).body
+        return mbid unless result == "not found\n"
+      end
     else
       return mb_results[0][:mbid]
     end
