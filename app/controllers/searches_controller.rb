85d034f4ffc2392c1eaf0067e10c4fdbae374b44
Timmy Huang <timmyhuang0404@gmail.com>
2014-05-27 12:00:00 -0400

    housekeeping

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -48,23 +48,23 @@ class SearchesController < ApplicationController
   end
 
   private
-  def save_band(result)
-    @band.mbid = result
-    @band.save!
-  end
+    def save_band(result)
+      @band.mbid = result
+      @band.save!
+    end
 
-  def save_concert(params)
-      @songs = params[:songs]
-      @concert_info = params[:concert].split(', ')
-      @venue = Venue.where(name: @concert_info[2], city: @concert_info[3], state: @concert_info[4]).first_or_create
-      @concert = Concert.where(date: @concert_info[0], venue_id: @venue.id).first_or_create
-      @concert_artist = ConcertArtist.where(concert_id: @concert.id, artist_id: @band.id).first_or_create
-      @date = @concert.date.strftime('%B %e %Y')
-      @tour = @concert_info[1]
-      @songs.each_with_index do |song_name, i|
-        next if song_name.nil?
-        song = Song.where(title: song_name, artist_id: @band.id).first_or_create
-        ConcertSong.where(concert_id: @concert.id, song_id: song.id, order: i).first_or_create
-      end
-  end
+    def save_concert(params)
+        @songs = params[:songs]
+        @concert_info = params[:concert].split(', ')
+        @venue = Venue.where(name: @concert_info[2], city: @concert_info[3], state: @concert_info[4]).first_or_create
+        @concert = Concert.where(date: @concert_info[0], venue_id: @venue.id).first_or_create
+        @concert_artist = ConcertArtist.where(concert_id: @concert.id, artist_id: @band.id).first_or_create
+        @date = @concert.date.strftime('%B %e %Y')
+        @tour = @concert_info[1]
+        @songs.each_with_index do |song_name, i|
+          next if song_name.nil?
+          song = Song.where(title: song_name, artist_id: @band.id).first_or_create
+          ConcertSong.where(concert_id: @concert.id, song_id: song.id, order: i).first_or_create
+        end
+    end
 end
