24e75d2de078ca9c9459594ffee3b06859aa0316
Timmy Huang <timmyhuang0404@gmail.com>
2014-05-26 23:52:27 -0400

    videos in concert page now displayed by setlist order

--- app/controllers/concerts_controller.rb
+++ app/controllers/concerts_controller.rb
@@ -3,6 +3,10 @@ class ConcertsController < ApplicationController
   def show
     @concert = Concert.find(params[:id])
     @venue = Venue.find(@concert.venue_id)
+    @concert_songs = @concert.concert_songs.sort_by(&:order)
+    puts "\n\n\nSORTED?"
+    p @concert_songs
+    puts "AFTER\n\n\n"
   end
 
 end
