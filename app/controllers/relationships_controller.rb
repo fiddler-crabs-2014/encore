54b3d8089d25dbc373c604c97f4a94b1bd13faad
Brendan <brendan.scarano@gmail.com>
2014-05-27 10:58:16 -0400

    deleted relationship create/destroy pages... not needed

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -6,13 +6,10 @@ class RelationshipsController < ApplicationController
     # respond_to do |format|
     #   format.js { @user = @followed}
     # end
-    @relationship = Relationship.all
   end
 
   def destroy
     relationship_id = Relationship.where(follower_id: current_user.id, followed_id: params[:id])[0].id
     Relationship.destroy(relationship_id)
-
-    @relationship = Relationship.all
   end
 end
