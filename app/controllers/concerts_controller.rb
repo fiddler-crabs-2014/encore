61d2d1aeb198f63181dd04c440e78f28b0d2c929
Timmy Huang <timmyhuang0404@gmail.com>
2014-05-26 23:57:44 -0400

    housekeeping

--- app/controllers/concerts_controller.rb
+++ app/controllers/concerts_controller.rb
@@ -4,9 +4,7 @@ class ConcertsController < ApplicationController
     @concert = Concert.find(params[:id])
     @venue = Venue.find(@concert.venue_id)
     @concert_songs = @concert.concert_songs.sort_by(&:order)
-    puts "\n\n\nSORTED?"
-    p @concert_songs
-    puts "AFTER\n\n\n"
+    @concert_songs
   end
 
 end
--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -1,5 +1,3 @@
-require 'json'
-
 class SearchesController < ApplicationController
 
   def index
