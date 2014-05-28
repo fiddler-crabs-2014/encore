37236f00564146741207aad350b2ba8e9aca2efa
Timmy Huang <timmyhuang0404@gmail.com>
2014-05-27 21:02:24 -0400

    optimize code and add error msg

--- app/controllers/concert_songs_controller.rb
+++ app/controllers/concert_songs_controller.rb
@@ -1,11 +1,19 @@
 class ConcertSongsController < ApplicationController
   def create
+    @concert = Concert.find(params[:concert_id])
+    @venue = @concert.venue
+    @concert_songs = @concert.concert_songs.sort_by(&:order)
+    @song_names = @concert_songs.map { |c_s| c_s.song.title }
     identifier = params[:video_link][/(?<=v=).*/]
-    artist = Artist.find_by(name: params[:artist_name])
-    song = Song.find_by(title: params[:song_name], artist_id: artist.id )
-    concert = Concert.find(params[:concert_id])
-    concert_song = ConcertSong.find_by(concert_id: concert.id, song_id: song.id)
-    Video.find_or_create_by(identifier: identifier, concert_song_id: concert_song.id)
-    redirect_to :back
+    artist = @concert.artists.find_by(name: params[:artist_name])
+    song = artist.songs.find_by(title: params[:song_name])
+    concert_song = @concert.concert_songs.find_by(song_id: song.id)
+    video = Video.find_or_initialize_by(identifier: identifier, concert_song_id: concert_song.id)
+    if video.save
+      redirect_to :back
+    else
+      flash[:warning] = "Unsuccessful submission. Please try again."
+      render "concerts/show"
+    end
   end
 end
