16bae5b86030457392abd9e1aeda427966f0700c
Howard O'Leary <howardeoleary@gmail.com>
2014-05-28 19:17:57 -0400

    set base styles, fixed landing page

--- db/schema.rb
+++ db/schema.rb
@@ -116,9 +116,9 @@ ActiveRecord::Schema.define(version: 20140528184223) do
   add_index "users", ["email"], name: "index_users_on_email", using: :btree
 
   create_table "venues", force: true do |t|
-    t.string   "name",       null: false
-    t.string   "city",       null: false
-    t.string   "state",      null: false
+    t.string   "name",       default: "n/a"
+    t.string   "city",       default: "n/a"
+    t.string   "state",      default: "n/a"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
