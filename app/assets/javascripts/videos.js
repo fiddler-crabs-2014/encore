765b79793b5a33367e31e9ab45beb648aabc482f
Timmy Huang <timmyhuang0404@gmail.com>
2014-05-29 09:25:14 -0400

    change highlight color to white

--- app/assets/javascripts/videos.js
+++ app/assets/javascripts/videos.js
@@ -19,7 +19,7 @@ function highlight(id) {
             "position": "absolute",
             "left": el.offset().left,
             "top": el.offset().top,
-            "background-color": "#DC7979",
+            "background-color": "white",
             "opacity": ".7",
             "z-index": "9999999"
         }).appendTo('body').fadeOut(1000).queue(function() { $(this).remove(); });
