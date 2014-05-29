5f2f073813f72147b633b07ebf38bd657259a081
Timmy Huang <timmyhuang0404@gmail.com>
2014-05-29 02:42:10 -0400

    highlight working

--- app/assets/javascripts/videos.js
+++ app/assets/javascripts/videos.js
@@ -1,9 +1,27 @@
-jQuery.fn.highlight = function() {
-    $(".video-box").on("click", function() {
-        $(this).css({
-                "background-color": "#ffff99",
-                "opacity": ".9"
-            })
-            .fadeOut(500);
+$(function() {
+  highlight();
+  $("#setlist a").on("click", function(){
+    var songID = $(this).attr("href");
+    $(songID).click();
+  })
+});
+
+
+function highlight() {
+   $(".video-box").on("click", function() {
+    var el = $(this);
+    $("<div/>")
+    .width(el.outerWidth())
+        .height(el.outerHeight())
+        .css({
+            "position": "absolute",
+            "left": el.offset().left,
+            "top": el.offset().top,
+            "background-color": "#ffff99",
+            "opacity": ".7",
+            "z-index": "9999999"
+        }).appendTo('body').fadeOut(1000).queue(function () { $(this).remove(); });
     });
 }
+
+
